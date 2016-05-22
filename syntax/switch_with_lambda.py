543300123a41d266515b6578b7f1cec7b9136567
YueTian Zeng <zengyuetian@YueTians-MacBook-Pro.local>
2016-05-22 09:43:53 +0800

    simulate switch with lambda

---
+++ syntax/switch_with_lambda.py
@@ -0,0 +1,22 @@
+# Python has no switch keyword
+# Here we simulate switch with lambda
+
+
+def a(s):
+    print s
+
+
+def switch(ch):
+    try:
+        {
+            '1': lambda: a("one"),
+            '2': lambda: a("two"),
+            '3': lambda: a("three")
+
+        }[ch]()
+    except KeyError:
+        a("key not found")
+
+
+switch("1")
+switch("a")
