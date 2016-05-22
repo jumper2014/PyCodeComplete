65499975cdc78bf108ae040e88e15e328a2fe377
YueTian Zeng <zengyuetian@YueTians-MacBook-Pro.local>
2016-05-22 08:41:38 +0800

    use for else syntax to find prime numbers

---
+++ syntax/for_else.py
@@ -0,0 +1,7 @@
+for n in range(2, 10000):
+    for i in range(2, n):
+        if n%i == 0:
+            break
+    else:
+        # loop through without finding
+        print(n, "is a prime number")
