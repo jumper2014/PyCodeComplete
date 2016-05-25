6ec0c12244f4e726f635777b5c2cc2896fd768f4
zengyuetian <zengyuetian@cloutropy.com>
2016-05-25 16:52:36 +0800

    使用for else语句查找素数

--- syntax/for_else.py
+++ syntax/for_else.py
@@ -1,6 +1,9 @@
+# coding=utf-8
+# 使用for else语句查找素数
+
 for n in range(2, 10000):
     for i in range(2, n):
-        if n%i == 0:
+        if n % i == 0:
             break
     else:
         # loop through without finding
