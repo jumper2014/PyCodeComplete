b7827c7810990ba945431d87e157c80ccdf9e893
zyt <ijumper@163.com>
2017-04-04 08:34:17 +0800

    add asterisk param sample code

--- Function/asterisk_param/asterisk_param_one.py
+++ Function/asterisk_param/asterisk_param_one.py
@@ -4,9 +4,11 @@
 # 星号作用于tuple/list/str前，意思是解包(unpack)tuple/list/str，
 # 解包后的tuple/list/str将不再是一个参数，而是每个元素都是一个函数参数，依次传递给函数
 
+
 def print_student_info(age, score):
-    print "age:", age
-    print "score:", score
+    print("age:", age)
+    print("score:", score)
+
 
 info_list = [18, 100]
 print_student_info(*info_list)
@@ -14,5 +16,3 @@ print_student_info(*info_list)
 # 如果此时星号被作用于一个dict前，那么只有dict的key会被解包，然后作为参数传递
 info_dict = {"age": 20, "score": 99}
 print_student_info(*info_dict)
-
-
--- Function/asterisk_param/asterisk_param_two.py
+++ Function/asterisk_param/asterisk_param_two.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 # 函数定义和调用时用两个星号
 
+
 def print_student_info1(**info):
-    print "age:", info.get("age", None)
-    print "score:", info.get("score", None)
+    print("age:", info.get("age", None))
+    print("score:", info.get("score", None))
 
 
 def print_student_info2(age, score):
-    print "age:", age
-    print "score:", score
-
+    print("age:", age)
+    print("score:", score)
 
 
 # 两个星号将会把字典完全unpack
