08e86f61ef118762b10221a8200d59fdb994a943
zyt <ijumper@163.com>
2017-05-01 16:32:46 +0800

    donot count __init__.py

--- count_file.py
+++ count_file.py
@@ -1,13 +1,14 @@
 # coding=utf-8
-#
+# count how many python file in this dir
 
 import os
 
 python_file_num = 0
 for root, dirs, files in os.walk("."):
     for fi in files:
-        if fi.find('.py') > -1:
+        # exclude init files
+        if fi.find('.py') > -1 and fi.find('__init__.py') < 0:
             python_file_num += 1
             print(fi)
 
-print(("\nTotal python file number is {0}.".format(python_file_num)))
+print("\nTotal python file number is {0}.".format(python_file_num))
