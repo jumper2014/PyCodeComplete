9eaba75f1f5373773a62e9c0983801ed43c099cb
penn <penn201500@gmail.com>
2017-05-08 22:18:29 +0800

    add index method to find characters in string

--- Strings/find_in_index.py
+++ Strings/find_in_index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # coding=utf-8
 # 在字符串中查找字串
 
@@ -5,6 +6,9 @@ if 'hello world,hello'.find('world') != -1:
     print('find')
 
 if 'world' in 'hello world,hello':
+    print('find')
+
+if 'hello world,hello'.index('world'):
     print('find')
 
 try:
