4c9e47ab786aebd759743feaad75a5594ab76cff
zyt <ijumper@163.com>
2017-06-02 19:00:17 +0800

    update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-for folder in zeus hera loki tapir thor breeze PyCodeComplete
+for folder in cherry zeus hera loki tapir thor breeze PyCodeComplete
         do
         	echo update $folder
         	cd $folder
