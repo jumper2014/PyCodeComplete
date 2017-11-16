b35e2a251102e4bcd06f167dcb97ecea345b64e0
zyt <ijumper@163.com>
2017-11-16 16:29:44 +0800

    remove pull.sh

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ..
-for folder in totoro cybertron phoebe jazz JaCodeComplete cherry zeus hera loki tapir thor breeze PyCodeComplete
-        do
-        	echo update $folder
-        	cd $folder
-            git pull
-            cd ..
-        done
