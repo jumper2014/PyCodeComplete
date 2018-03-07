d236aa76301c71d173c5f7c06ac57f39d1361e05
zengyuetian <zengyuetian@cloutropy.com>
2018-03-07 14:31:13 +0800

    add csv sample

--- practice/lib/csv/20180307/csv_sample.py
+++ practice/lib/csv/20180307/csv_sample.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
 # coding=utf-8
+# author: zengyuetian
+# 演示csv读写
+
 import csv
+import time
 
 # write stocks data as comma-separated values
 writer = csv.writer(open('stock.csv', 'wb', buffering=0))
@@ -14,4 +19,24 @@ stocks = csv.reader(open('stock.csv', 'rb'))
 status_labels = {-1: 'down', 0:'unchanged', 1:'up'}
 for ticker, name, price, change, pct in stocks:
     status = status_labels[cmp(float(change), 0.0)]
-    print '%s is %s (%s%%)' %(name, status, pct)
+    print('%s is %s (%s%%)' %(name, status, pct))
+
+
+def write_by_csv_lib(datas):
+    start = time.time()
+    writer = csv.writer(open('csv_lib.csv', 'wb'))
+    writer.writerows(datas)
+    print('write_by_csv_lib cost {0} seconds'.format(time.time()-start))
+
+
+def write_by_file_lib(datas):
+    start = time.time()
+    with open('file_lib.csv', 'wb') as f:
+        for data in datas:
+            f.write('{0},{1}\n'.format(data[0], data[1]))
+    print('write_by_file_lib cost {0} seconds'.format(time.time()-start))
+
+if __name__ == '__main__':
+    datas = [(x, x) for x in range(1000)]
+    write_by_csv_lib(datas)
+    write_by_file_lib(datas)
