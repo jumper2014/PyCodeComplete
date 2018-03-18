93cadef4efbedf26d592d84ef6ecc1ab3272a26d
zyt <ijumper@163.com>
2018-03-18 16:27:47 +0800

    add pickle sample

---
+++ practice/lib/pickle/pickle_sample.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author: zengyuetian
+
+import pickle
+
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print self.name + "_" + str(self.age)
+
+
+# 序列化到文件
+user = User("Python", 20)
+user.show()
+f = open('user.pkl', 'w')
+pickle.dump(user, f)
+f.close()
+
+# 反序列化到内存
+f = open('user.pkl', 'r')
+user1 = pickle.load(f)
+f.close()
+user1.show()
