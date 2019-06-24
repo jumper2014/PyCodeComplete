bef1a997d3d9e61ffbc1937a86305b2f0b8683a5
zyt <ijumper@163.com>
2019-06-24 23:36:06 +0800

    abstract functions

--- Others/Modules/json/xml_json_checker.py
+++ Others/Modules/json/xml_json_checker.py
@@ -13,21 +13,21 @@ import optparse
 
 
 class XmlJsonChecker(object):
-
     def __init__(self, api, direction):
         self.in_format, self.out_format = direction.split('2')
         self.ini = "./{0}/{1}/node.ini".format(api, direction)
         self.trans = "./{0}/{1}/trans.xml".format(api, direction)
         self.input_file = "./{0}/{1}/input.{2}".format(api, direction, self.in_format)
         self.expected_file = "./{0}/{1}/expected.{2}".format(api, direction, self.out_format)
+        self.temp_file = "./{0}/{1}/temp.xml".format(api, direction)
         self.real = None
         self.expected = None
 
-    def xml_trans(self):
+    def handle_xml_tree(self, xml_file):
         f = open(self.ini, 'r')
         lines = f.readlines()
 
-        tree = ET.parse(self.input_file)
+        tree = ET.parse(xml_file)
         root = tree.getroot()
 
         for line in lines:
@@ -44,34 +44,39 @@ class XmlJsonChecker(object):
                     print("find key, delete")
 
         tree.write(self.trans)
+
+    def xml_trans(self):
+
+        self.handle_xml_tree(self.input_file)
+
+        # tree = ET.parse(self.input_file)
+        # root = tree.getroot()
+        #
+        # for line in lines:
+        #     line = line.strip()
+        #     nodes = line.split('/')
+        #     child_node = nodes[-1]
+        #     length = len(child_node)
+        #     father_node = line[: -(length + 1)]
+        #     print("start to handle node:", father_node, child_node)
+        #
+        #     for e in root.findall(father_node):
+        #         for el in e.findall(child_node):
+        #             e.remove(el)
+        #             print("find key, delete")
+        #
+        # tree.write(self.trans)
 
     def json_trans(self):
         json_file = open(self.input_file, 'r')
         json_str = json.load(json_file)
         xml = xmltodict.unparse(json_str, full_document=False)
 
-        f = open('temp.xml', 'w')
+        f = open(self.temp_file, 'w')
         f.write(xml)
         f.close()
-        tree = ET.parse('temp.xml')
-        root = tree.getroot()
 
-        f = open(self.ini, 'r')
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            nodes = line.split('/')
-            child_node = nodes[-1]
-            length = len(child_node)
-            father_node = line[: -(length + 1)]
-            print("start to handle node:", father_node, child_node)
-
-            for e in root.findall(father_node):
-                for el in e.findall(child_node):
-                    e.remove(el)
-                    print("find key, delete")
-
-        tree.write(self.trans)
+        self.handle_xml_tree(self.temp_file)
 
     def xml_to_json(self):
         f = open(self.trans, 'r')
