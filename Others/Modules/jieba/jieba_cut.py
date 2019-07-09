c18bd8304b9dfa83bfdda1bd8419ea19772513de
zyt <ijumper@163.com>
2019-07-09 23:52:57 +0800

    add word cut sample

--- Others/Modules/jieba/jieba_cut.py
+++ Others/Modules/jieba/jieba_cut.py
@@ -3,14 +3,13 @@
 # author: zengyuetian
 
 import jieba
+import pkuseg
 
 if __name__ == '__main__':
-    seg_list = jieba.cut("绿子在电话的另一头久久默然不语，如同全世界的细雨落在全世界所有的草坪上一般的沉默在持续。",
-                         cut_all=False, HMM=True)
+    text = "绿子在电话的另一头久久默然不语，如同全世界的细雨落在全世界所有的草坪上一般的沉默在持续。"
+    seg_list = jieba.cut(text,  cut_all=False, HMM=True)
     print("Cut result is: " + "/".join(seg_list))
 
-    import pkuseg
-
     seg = pkuseg.pkuseg()  # 以默认配置加载模型
-    text = seg.cut('"绿子在电话的另一头久久默然不语，如同全世界的细雨落在全世界所有的草坪上一般的沉默在持续。')  # 进行分词
-    print(text)
+    text = seg.cut(text)  # 进行分词
+    print("Cut result is: " + "/".join(text))
